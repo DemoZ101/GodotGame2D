a916cf53af5b0413da801e8448730a4048af3ad2
DemoZ101 <66673111+DemoZ101@users.noreply.github.com>
2021-01-29 09:54:46 +0700

    pause menu, set Tilemap collision

--- Godot2DPlatformer/ui/Pause.gd
+++ Godot2DPlatformer/ui/Pause.gd
@@ -1,6 +1,22 @@
-extends Control
+extends CanvasLayer
+
+func _ready():
+	set_visible(false)
 
 func _input(event):
 	if event.is_action_pressed("pause"):
-		var new_pause_state = not get_tree().paused
-		get_tree().paused = new_pause_state
+		set_visible(!get_tree().paused)
+		get_tree().paused = !get_tree().paused
+
+func set_visible(is_visible):
+	for node in get_children():
+		node.visible = is_visible
+
+func _on_Resume_pressed():
+	get_tree().paused = false
+	set_visible(false)
+
+func _on_Restart_pressed():
+	get_tree().paused = false
+	set_visible(false)
+	GameState.restart()
